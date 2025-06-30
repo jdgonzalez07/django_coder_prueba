e7f511e29ed66b10f453ed701bd2a40d309293ca
Juan Gonzalez <jdgonzalez2017@hotmail.com>
2025-06-30 16:10:08 -0300

    ajuste

--- inicio/models.py
+++ inicio/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Auto(models.Model):
     marca = models.CharField(max_length=20)
     modelo = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return f'{self.marca} {self.modelo} {self.id}'
