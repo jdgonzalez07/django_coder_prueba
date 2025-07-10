df47bf2a6a385bc3f1bcbcb9f5c217e2afe019d1
Juan Gonzalez <jdgonzalez2017@hotmail.com>
2025-07-10 16:50:30 -0300

    Avances

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from inicio.views import saludo, saludo_template, saludo_con_cargador, saludo_con_render, condicion_y_bucle, inicio, crear_auto
 
+app_name = 'inicio'
 
 urlpatterns = [
-    path("", inicio ),
+    path("", inicio, name='inicio' ),
     # path('admin/', admin.site.urls),
     # path('saludo/<str:nombre>/<str:apellido>/', saludo),
     # path('saludo/<str:nombre>/<str:apellido>/', saludo_template),
     # path('saludo/<str:nombre>/<str:apellido>/', saludo_con_cargador),
-    path('saludo/<str:nombre>/<str:apellido>/', saludo_con_render),
-    path('template-prueba/', condicion_y_bucle),
+    path('saludo/<str:nombre>/<str:apellido>/', saludo_con_render, name='saludo_template'),
+    path('template-prueba/', condicion_y_bucle, name='template_prueba'),
     path('auto/crear/<str:marca>/<str:modelo>', crear_auto )
 ]
